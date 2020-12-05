7b0916490789be4c0930dc7253fb87565747e5f7
Supuni22 <supunimanamperi@gmail.com>
2020-12-05 11:05:38 +0530

    Added routes

--- auth-demo/src/App.js
+++ auth-demo/src/App.js
@@ -1,19 +1,26 @@
 import { Container } from 'react-bootstrap';
 import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
 import { AuthProvider } from './context/AuthContext';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
   return (
-    <AuthProvider>
        <Container 
         className='d-flex align-items-center justify-content-center' 
         style={{minHeight:'100vh'}}
       >
         <div className='w-100' style={{maxWidth:'400px'}}>
-          <SignUp/>
+         <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path='/' component={Dashboard}/>
+                <Route path='/signup' component={SignUp}/>
+              </Switch>
+            </AuthProvider>
+         </Router>
         </div>
     </Container>
-    </AuthProvider>
   );
 }
 
