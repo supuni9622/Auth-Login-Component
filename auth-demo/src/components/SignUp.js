af7e130be9c3d14055d6122c4bfc4f019132bbec
Supuni22 <supunimanamperi@gmail.com>
2020-12-05 10:02:22 +0530

    Added validations

--- auth-demo/src/components/SignUp.js
+++ auth-demo/src/components/SignUp.js
@@ -1,5 +1,5 @@
-import React, {useRef, useCallback} from 'react';
-import {Card, Form, Button} from 'react-bootstrap';
+import React, {useRef, useCallback, useState} from 'react';
+import {Card, Form, Button, Alert} from 'react-bootstrap';
 import {useAuth} from '../context/AuthContext';
 
 const SignUp = () => {
@@ -7,14 +7,32 @@ const SignUp = () => {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
+    const {signUp, currentUser} = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const {signUp} = useAuth();
-
-    const handleSubmit = useCallback((e) => {
+    const handleSubmit = useCallback(async(e) => {
         e.preventDefault();
-        
-        signUp(emailRef.current.value, passwordRef.current.value);
 
+        // Validations
+        if(passwordRef.current.value !== passwordConfirmRef.current.value){
+            return setError('Passwords don not match');
+        }
+        if(passwordRef.current.value.length < 6 ){
+            return setError('Password should have at least 6 characters');
+        }
+
+        try{
+            setError('');
+            setLoading(true);
+            await signUp(emailRef.current.value, passwordRef.current.value);
+            
+        } catch {
+            setError('Failed to create an account');
+        }finally{
+            setLoading(false);
+        }
+        
         }, [signUp]);
 
     return (
@@ -22,7 +40,10 @@ const SignUp = () => {
           <Card>
               <Card.Body>
                 <h2 className='text-center mb-4'>Sign Up</h2>
-                <Form>
+                {/* {JSON.stringify(currentUser)} */}
+                {currentUser && currentUser.email}
+                {error && <Alert variant='danger'>{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
                   <Form.Group id='email'>
                     <Form.Label>Email</Form.Label>
                     <Form.Control type='email' ref={emailRef} required/>
@@ -35,7 +56,7 @@ const SignUp = () => {
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control type='password' ref={passwordConfirmRef} required/>
                   </Form.Group>
-                <Button className='w-100' variant='info' type='submit'>Sign Up</Button>
+                <Button className='w-100' variant='info' type='submit' disabled={loading}>Sign Up</Button>
               </Form>
               </Card.Body>
           </Card>
