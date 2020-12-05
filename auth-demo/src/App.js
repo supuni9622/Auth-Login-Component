f4e495ddb39f4b8ee646599a701ab89aaa3ec8f1
Supuni22 <supunimanamperi@gmail.com>
2020-12-05 09:04:47 +0530

    Created signUp form

--- auth-demo/src/App.js
+++ auth-demo/src/App.js
@@ -1,10 +1,16 @@
+import { Container } from 'react-bootstrap';
+import SignUp from './components/SignUp';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Auth Demo
-    </div>
+     <Container 
+        className='d-flex align-items-center justify-content-center' 
+        style={{minHeight:'100vh'}}
+      >
+        <div className='w-100' style={{maxWidth:'400px'}}>
+          <SignUp/>
+        </div>
+    </Container>
   );
 }
 
